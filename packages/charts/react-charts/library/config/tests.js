82b3072b9fa20f07fbd5c5a05dd57026ed86e4ef
Atishay Jain (atisjai) <98592573+AtishayMsft@users.noreply.github.com>
2025-05-31 03:19:53 +0000

    Enable tests for chart-utilities. Remove enzyme adapter

--- packages/charts/react-charts/library/config/tests.js
+++ packages/charts/react-charts/library/config/tests.js
@@ -1,7 +1,4 @@
 /** Jest test setup file. */
-const { configure } = require('enzyme');
-require('@testing-library/jest-dom');
-const Adapter = require('@wojtekmaj/enzyme-adapter-react-17');
 
-// Configure enzyme.
-configure({ adapter: new Adapter() });
+const env = 'PROD';
+module.exports = env;
